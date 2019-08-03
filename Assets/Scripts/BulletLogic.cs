ae5194a61929b4ebc704b523daf90857788f6d90
Ran Crump <rancrump18@gmail.com>
2019-08-02 21:21:52 -0500

    + Bullet Bouncing, Shooting, Aiming

---
+++ Assets/Scripts/BulletLogic.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletLogic : MonoBehaviour
+{
+    public Rigidbody rb;
+    void OnCollisionEnter(Collision coll)
+    {        
+        if (coll.gameObject.tag == "Wall")
+        {            
+            var dir = Vector3.Reflect(rb.velocity, coll.contacts[0].normal);
+            rb.velocity = dir * 1.5f;            
+            transform.rotation = Quaternion.Euler(rb.velocity);
+            rb.angularVelocity = Vector3.zero;
+        }
+    }
+}
---
+++ Assets/Scripts/ShootingController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootingController : MonoBehaviour
+{
+    public GameObject bullet;
+    public GameObject shootingPoint;
+    public float bulletSpeed;
+    public LayerMask lm;
+    public float YOffset = .5f;
+    public GameObject player;
+    private int ammo = 99;
+    public Camera cam;
+
+    void Start()
+    {
+        lm = ~lm;
+    }
+
+    void Update()
+    {
+           
+        var ray = cam.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out RaycastHit hit))
+        {
+            Vector3 pos = hit.point;
+            pos.y = transform.position.y;
+            transform.LookAt(pos);            
+            if (Input.GetMouseButtonDown(0) && ammo > 0)
+            {
+                Shoot(hit.point);   
+                ammo--; 
+            }            
+        }
+    }
+
+    void Shoot(Vector3 pos)
+    {
+        pos.y += YOffset;
+        GameObject Bullet = Instantiate(bullet, shootingPoint.transform.position, transform.rotation);
+        Bullet.name = "Bullet";
+        Bullet.GetComponent<Rigidbody>().velocity = bulletSpeed * Time.deltaTime * transform.forward;
+    }
+}
