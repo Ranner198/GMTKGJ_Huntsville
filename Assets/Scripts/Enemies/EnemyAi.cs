c2782ddc9b6aea99cc7fde3d6f1069f6c42aa57c
RandomProduct <mikejrea95@gmail.com>
2019-08-02 21:46:58 -0500

    Enemy Kill and Respawn

--- Assets/Scripts/Enemies/EnemyAi.cs
+++ Assets/Scripts/Enemies/EnemyAi.cs
@@ -5,6 +5,7 @@ using UnityEngine.AI;
 
 public class EnemyAi : MonoBehaviour {
 
+    #region Multiton
     public static List<EnemyAi> instances = new List<EnemyAi>();
 
     private void Awake() {
@@ -14,21 +15,41 @@ public class EnemyAi : MonoBehaviour {
 
         instances.Add(this);
     }
+    #endregion
 
     public Transform player;
     private Vector3 playerLastPos;
-    NavMeshAgent agent;
+    private NavMeshAgent agent;
+
+    private Transform startTransform;
 
     private void Start() {
         agent = GetComponent<NavMeshAgent>();
         agent.destination = player.position;
         playerLastPos = player.position;
+
+        startTransform = transform;
     }
 
     private void Update() {
         if (player.position != playerLastPos) {
             agent.destination = player.position;
             playerLastPos = player.position;
+        }
+    }
+
+    public void Respawn() {
+        transform.position = startTransform.position;
+        transform.rotation = startTransform.rotation;
+    }
+
+    public void Kill() {
+        transform.position = new Vector3(-1000, -1000, -1000);
+    }
+
+    public void OnDestroy() {
+        if (instances.Contains(this)) {
+            instances.Remove(this);
         }
     }
 }
