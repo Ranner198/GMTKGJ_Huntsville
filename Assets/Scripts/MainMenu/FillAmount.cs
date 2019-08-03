c60bb96838029a91eed351e65ea4f7965c45572b
Ran Crump <rancrump18@gmail.com>
2019-08-03 12:05:51 -0500

    + mAIN mENU

---
+++ Assets/Scripts/MainMenu/FillAmount.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class FillAmount : MonoBehaviour
+{
+    public Image image;
+    bool finished = true;
+    float targetAmt;
+    public float amt;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            finished = false;
+            if (targetAmt == 0)
+            {
+                targetAmt = 1;
+                amt = 0;
+            }
+            else
+            {
+                targetAmt = 0;    
+                amt = 1;            
+            }
+        }
+
+        if (!finished)
+        {
+            if (targetAmt == 1)
+                amt += Time.deltaTime;
+            else
+                amt -= Time.deltaTime;
+
+            image.fillAmount = amt;
+
+            if (amt > 1 || amt < 0)
+            {
+                finished = true;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/MainMenu/MainMenuMoveScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuMoveScript : MonoBehaviour {
+
+    public float speed;
+
+    public Vector3 offset;
+
+    private void Awake()
+    {
+        offset = transform.position;
+    }
+
+    void Update () {
+
+        Vector2 dir = Camera.main.ScreenToViewportPoint(Input.mousePosition);
+
+        Vector3 fixPos = new Vector3(dir.x + offset.x, dir.y + offset.y, transform.position.z);
+
+        transform.position = Vector3.Slerp(transform.position, fixPos, Time.deltaTime * speed * 60);
+    }
+}
