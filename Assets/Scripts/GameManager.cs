1b75847c76cdc0b51ec9b82171f78937c82743fa
RandomProduct <mikejrea95@gmail.com>
2019-08-03 16:32:26 -0500

    Explosion

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,17 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
-public class GameManager : MonoBehaviour
-{
-	//singleton
-	public static GameManager instance;
+public class GameManager : MonoBehaviour {
+    //singleton
+    public static GameManager instance;
 
 
     public LevelObject activeLevel;
     private GameObject level;
     public List<LevelObject> levelList = new List<LevelObject>();
     public List<EnemyAi> enemyAi = new List<EnemyAi>();
-    public PlayerMovement player; 
+    public PlayerMovement player;
     public int levelsCompleted = 0;
     public float totalTimeSpent = 0f;
     public float levelTimeSpent = 0f;
@@ -21,40 +20,39 @@ public class GameManager : MonoBehaviour
     public int totalKills = 0;
     public bool doorOpened;
 
-    private void Awake(){
-    	instance = this;
+    private void Awake() {
+        instance = this;
     }
 
 
-    private void Start(){
-    	player = PlayerMovement.playerInstance;
-    	NewLevel();
+    private void Start() {
+        player = PlayerMovement.playerInstance;
+        NewLevel();
     }
 
-	public void Kill()
-	{
+    public void Kill() {
         currentKills++;
-	}
-
-    private void Update(){
-		
-    	levelTimeSpent += Time.deltaTime;
-    	totalTimeSpent += Time.deltaTime;
-
-    	//controls checking
-    	if(Input.GetKeyDown(KeyCode.R))
-    		ResetLevel();
-    	if(Input.GetKeyDown(KeyCode.K)){
-    		enemyAi[Random.Range(0,enemyAi.Count)].Kill();
-    	}
-    	if(Input.GetKeyDown(KeyCode.N)){
-    		CompleteLevel();
-    	}
     }
 
-    int currentLevelIndex = -1;
+    private void Update() {
 
-    public void NewLevel(){
+        levelTimeSpent += Time.deltaTime;
+        totalTimeSpent += Time.deltaTime;
+
+        //controls checking
+        if (Input.GetKeyDown(KeyCode.R))
+            ResetLevel();
+        if (Input.GetKeyDown(KeyCode.K)) {
+            enemyAi[Random.Range(0, enemyAi.Count)].Kill();
+        }
+        if (Input.GetKeyDown(KeyCode.N)) {
+            CompleteLevel();
+        }
+    }
+
+    int currentLevelIndex = 0;
+
+    public void NewLevel() {
 
         doorOpened = false;
 
@@ -71,44 +69,40 @@ public class GameManager : MonoBehaviour
     		level = activeLevel.levelPrefab;
     	}*/
 
-        if(currentLevelIndex == -1) {
-            level = activeLevel.levelPrefab;
-        }
-        else {
+        if (level != null)
             Destroy(level);
-            activeLevel = levelList[currentLevelIndex];
-            level = Instantiate(activeLevel.levelPrefab, Vector3.zero, Quaternion.identity);
-        }
+        activeLevel = levelList[currentLevelIndex];
+        level = Instantiate(activeLevel.levelPrefab, Vector3.zero, Quaternion.identity);
         currentLevelIndex++;
 
-    	//get enemies
-    	enemyAi = EnemyAi.instances;
-    	totalKills += currentKills;
-    	currentKills = 0;
-    	player.ResetPosition(activeLevel.spawnPoint);
+        //get enemies
+        enemyAi = EnemyAi.instances;
+        totalKills += currentKills;
+        currentKills = 0;
+        player.ResetPosition(activeLevel.spawnPoint);
     }
-    public void CompleteLevel(){
-    	levelsCompleted += 1;
-    	levelTimeSpent = 0f;
-    	NewLevel();
+    public void CompleteLevel() {
+        levelsCompleted += 1;
+        levelTimeSpent = 0f;
+        NewLevel();
     }
 
-    public void StartBulletTime(){
+    public void StartBulletTime() {
         StartCoroutine(BulletTime(0.75f));
     }
 
-    private void ResetLevel(){
-    	for(int i=0;i<enemyAi.Count;i++){
-    		//reset enemies
-    		enemyAi[i].Respawn();
-    	}
-    	levelTimeSpent = 0f;
-    	currentKills = 0;
-
+    private void ResetLevel() {
+        for (int i = 0; i < enemyAi.Count; i++) {
+            //reset enemies
+            enemyAi[i].Respawn();
+        }
+        levelTimeSpent = 0f;
+        currentKills = 0;
+        player.ResetPosition(activeLevel.spawnPoint);
     }
 
     public bool OnLastKill() {
-        if(currentKills == activeLevel.killsRequired - 1) {
+        if (currentKills == activeLevel.killsRequired - 1) {
             return true;
         }
         else {
@@ -117,13 +111,13 @@ public class GameManager : MonoBehaviour
     }
 
     public void SetTimeScaleBasedOnBullet(float distanceBetweenBulletAndEnemy) {
-        if(distanceBetweenBulletAndEnemy < 0.5f) {
+        if (distanceBetweenBulletAndEnemy < 0.5f) {
             Time.timeScale = 0.01f;
         }
-        else if(distanceBetweenBulletAndEnemy < 1f) {
+        else if (distanceBetweenBulletAndEnemy < 1f) {
             Time.timeScale = 0.1f;
         }
-        else if(distanceBetweenBulletAndEnemy < 1.5f) {
+        else if (distanceBetweenBulletAndEnemy < 1.5f) {
             Time.timeScale = 0.2f;
         }
         else {
@@ -133,22 +127,20 @@ public class GameManager : MonoBehaviour
         print(Time.timeScale);
     }
 
-    private IEnumerator BulletTime(float timer){
-        while(timer > 0) {
+    private IEnumerator BulletTime(float timer) {
+        while (timer > 0) {
             timer -= Time.deltaTime;
-            if(currentKills >= activeLevel.killsRequired) {
+            if (currentKills >= activeLevel.killsRequired) {
                 timer = -1f;
             }
             yield return null;
         }
         //Camera.main.transform.position = new Vector3(0, 10, -2);
-    	//Time.timeScale = 1f;
+        //Time.timeScale = 1f;
     }
 
-    private void OpenDoor()
-    {
-        if (currentKills == activeLevel.killsRequired)
-        {
+    private void OpenDoor() {
+        if (currentKills == activeLevel.killsRequired) {
             doorOpened = true;
         }
     }
@@ -161,8 +153,8 @@ public class GameManager : MonoBehaviour
 }
 
 [System.Serializable]
-public class LevelObject{
-	public GameObject levelPrefab;
-	public int killsRequired;
-	public Vector2 spawnPoint;
+public class LevelObject {
+    public GameObject levelPrefab;
+    public int killsRequired;
+    public Vector2 spawnPoint;
 }
