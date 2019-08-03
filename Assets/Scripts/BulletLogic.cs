4fc944678093a302dd633dc3888041edbacc2053
Ran Crump <rancrump18@gmail.com>
2019-08-02 22:52:46 -0500

    + Reworking bullet bounce for consistancy

--- Assets/Scripts/BulletLogic.cs
+++ Assets/Scripts/BulletLogic.cs
@@ -7,10 +7,11 @@ public class BulletLogic : MonoBehaviour
     public Rigidbody rb;
     void OnCollisionEnter(Collision coll)
     {        
+        float speed = rb.velocity.magnitude;
         if (coll.gameObject.tag == "Wall")
         {            
-            var dir = Vector3.Reflect(rb.velocity, coll.contacts[0].normal);
-            rb.velocity = dir * 1.5f;            
+            var dir = Vector3.Reflect(rb.velocity.normalized, coll.contacts[0].normal);
+            rb.velocity = dir * speed * 2;            
             transform.rotation = Quaternion.Euler(rb.velocity);
             rb.angularVelocity = Vector3.zero;
         }
