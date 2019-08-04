b7b16773c066bc879c47001c1c6648cd49779f21
RandomProduct <mikejrea95@gmail.com>
2019-08-03 23:49:32 -0500

    Added Fizzle Out sound effect to bullets

---
+++ Assets/DestroyOnAudioEnd.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOnAudioEnd : MonoBehaviour {
+    IEnumerator Start() {
+        AudioSource source = GetComponent<AudioSource>();
+        while(source.isPlaying == false) {
+            yield return null;
+        }
+        while (source.isPlaying) {
+            yield return null;
+        }
+        Destroy(gameObject);
+    }
+}
